58a901038f8b51128022d99d06771a89226954f5
Faiz <sheikhfaiz7861@gmail.com>
2024-10-21 20:01:36 +0530

    j

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+
+const data = [
+  {
+    id: 1,
+    name: "abc",
+  },
+  {
+    id: 2,
+    name: "xyz",
+  },
+  {
+    id: 3,
+    name: "faiz",
+  },
+];
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key={"relativeList"}>
+        {data.map((curr) => {
+          return <li key={curr.id}>{curr.name}</li>;
+        })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
